578e9ad6692b0d544b1f854764c64cf68bbdf066
Sasha Butenkova <butenkova66@gmail.com>
2025-10-11 22:02:12 +0300

    Fix build for Netify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+	plugins: [react()],
+	base: '/king-charles-spaniel/', // 👈 сюда то же имя, что и в homepage
+})
