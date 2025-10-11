6f3b692ac89572c57cb1ba94c2578bef3ce567cd
Sasha Butenkova <butenkova66@gmail.com>
2025-10-11 22:05:42 +0300

    Fix build for Netify

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 	plugins: [react()],
-	base: '/king-charles-spaniel/', // 👈 сюда то же имя, что и в homepage
+	base: '/', // 👈 сюда то же имя, что и в homepage
 })
